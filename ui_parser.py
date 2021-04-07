cae0c80e9fd18e5d75899e05c3bc3bef84a9e077
shirawein <sbmw15@gmail.com>
2021-04-07 17:55:41 -0400

    Update ui_parser.py

--- ui_parser.py
+++ ui_parser.py
@@ -12,8 +12,6 @@ parser = argparse.ArgumentParser(description='List the content of a folder')
 parser.add_argument('vars', nargs='+')
 opts = parser.parse_args()	
 
-print(opts.vars)
-
 all_input = ' '.join(opts.vars)
 
 print(all_input)
