35e7cc7ea4f76cc0ed5afd69939da5f7280f8896
shirawein <sbmw15@gmail.com>
2021-04-07 16:54:05 -0400

    Adding first command line interface step

--- parser.py
+++ parser.py
@@ -11,18 +11,20 @@ import sys
 my_parser = argparse.ArgumentParser(description='List the content of a folder')
 
 # Add the arguments
-my_parser.add_argument('Path',
-                       metavar='path',
+my_parser.add_argument('Operation',
+                       metavar='Operation',
                        type=str,
-                       help='the path to list')
+                       help='create_table, drop_table, create_index, drop_index, select, insert, delete, update')
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
 
-input_path = args.Path
+operation = args.Operation
 
-if not os.path.isdir(input_path):
-    print('The path specified does not exist')
+types_of_operations = ["create_table", "drop_table", "create_index", "drop_index", "select", "insert", "delete", "update"]
+
+if not operation in types_of_operations:
+    print(' The operation must be one of the following: "create_table", "drop_table", "create_index", "drop_index", "select", "insert", "delete", "update" ')
     sys.exit()
 
 print('\n'.join(os.listdir(input_path)))
