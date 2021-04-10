5f789120c6af801d51632ac5f0e0e5d2bfb43984
Hrishikesh Kulkarni <hrishikeshkulkarni@Hrishikeshs-Air.hsd1.dc.comcast.net>
2021-04-10 14:10:02 -0400

    delete from table with multiple constraints

--- csv_util.py
+++ csv_util.py
@@ -7,6 +7,34 @@ def merge(list1, list2):
     merged_list = [(list1[i], list2[i]) for i in range(0, len(list1))]
     return merged_list
 
+def get_loc(colname, colname_list):
+	i = -1
+	for col in colname_list:
+		i += 1
+		if colname == col:
+			return(i)
+
+def condition_function(obj1, sign, obj2):
+	if sign == '=':
+		if obj1 == obj2:
+			return True
+	elif sign == '<':
+		if obj1 < obj2:
+			return True
+	elif sign == '>':
+		print(obj1)
+		print(obj2)
+		print(obj1 > obj2)
+		if obj1 > obj2:
+			return True
+	elif sign == '<=':
+		if obj1 <= obj2:
+			return True
+	elif sign == '>=':
+		if obj1 >= obj2:
+			return True
+	return False
+
 def _init_storage():
 	with open("table_data.csv", 'a', newline='') as file:
 		writer = csv.writer(file, delimiter = ",")
@@ -113,14 +141,56 @@ def _insert(table_name, colname_list, value_list):
 		writer = csv.writer(file, delimiter = ",")
 		writer.writerow(list(map(list, zip(*sorted_tuple_list)))[1])
 
+def _delete(table_name, colname_list, condition_list, value_list):
+	
+	file_name= table_name + ".csv"
 
+	# check if table exists
+	flag = 0
+	with open('table_data.csv') as readFile:
+		reader = csv.reader(readFile, delimiter=',')
+		for row in reader:
+			if row[0] == table_name:
+				flag = 1
+				break
+	if flag == 0 or not os.path.exists(file_name):
+		print("The table: {} does not exist!".format(table_name))
+		return
+
+	# get the sorted complete col names
+	with open(file_name, 'r') as readFile:
+		reader = csv.reader(readFile)
+		for row in reader:
+			sorted_colname_list = row
+			break	
+
+	lines = list()
+	with open(file_name, 'r') as readFile:
+		reader = csv.reader(readFile)
+		first_row = True
+		for row in reader:
+			true_flag = 0
+			if(not first_row):
+				for i in range(0, len(colname_list)):
+					loc = get_loc(colname_list[i], sorted_colname_list)
+					if not condition_function(row[i], condition_list[i], str(value_list[i])):
+						true_flag = 1
+			else:
+				first_row = False
+				true_flag = 1
+			if true_flag == 1:
+				lines.append(row)
+
+	with open(file_name, 'w') as writeFile:
+		writer = csv.writer(writeFile)
+		writer.writerows(lines)
 
 
 #_init_storage()
 #_drop_table('table_name_test')
 #_create_table('table_name_test', 0, 2, ['int', 'varchar 20'], ['idd', 'namee'], ['idd', 'namee'])
-_insert('table_name_test', ['idd', 'namee'], [12, 'asef'])
-
+#_insert('table_name_test', ['idd', 'namee'], [10, 'asdf'])
+_delete('table_name_test', ['idd', 'namee'], ['>', '='], [12, 'asdf'])
 
 
 
