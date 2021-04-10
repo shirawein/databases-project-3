52267e414fe68533a42a593b605ab39e35c06d25
Hrishikesh Kulkarni <hrishikeshkulkarni@Hrishikeshs-Air.hsd1.dc.comcast.net>
2021-04-10 14:29:45 -0400

    update in table independent of order of column names

--- csv_util.py
+++ csv_util.py
@@ -173,7 +173,7 @@ def _delete(table_name, colname_list, condition_list, value_list):
 			if(not first_row):
 				for i in range(0, len(colname_list)):
 					loc = get_loc(colname_list[i], sorted_colname_list)
-					if not condition_function(row[i], condition_list[i], str(value_list[i])):
+					if not condition_function(row[loc], condition_list[i], str(value_list[i])):
 						true_flag = 1
 			else:
 				first_row = False
@@ -185,12 +185,62 @@ def _delete(table_name, colname_list, condition_list, value_list):
 		writer = csv.writer(writeFile)
 		writer.writerows(lines)
 
+def _update(table_name, colname_list, condition_list, value_list, new_value_list):
+	
+	file_name= table_name + ".csv"
+
+	# check if table exists
+	flag = 0
+	with open('table_data.csv') as readFile:
+		reader = csv.reader(readFile, delimiter=',')
+		for row in reader:
+			if row[0] == table_name:
+				flag = 1
+				break
+	if flag == 0 or not os.path.exists(file_name):
+		print("The table: {} does not exist!".format(table_name))
+		return
+
+	# get the sorted complete col names
+	with open(file_name, 'r') as readFile:
+		reader = csv.reader(readFile)
+		for row in reader:
+			sorted_colname_list = row
+			break	
+
+	lines = list()
+	with open(file_name, 'r') as readFile:
+		reader = csv.reader(readFile)
+		first_row = True
+		for row in reader:
+			true_flag = 0
+			if(not first_row):
+				for i in range(0, len(colname_list)):
+					loc = get_loc(colname_list[i], sorted_colname_list)
+					if not condition_function(row[loc], condition_list[i], str(value_list[i])):
+						true_flag = 1
+			else:
+				first_row = False
+				true_flag = 1
+			if true_flag == 1:
+				lines.append(row)
+			else:
+				for i in range(0, len(colname_list)):
+					loc = get_loc(colname_list[i], sorted_colname_list)
+					row[loc] = new_value_list[i]
+				lines.append(row)
+
+	with open(file_name, 'w') as writeFile:
+		writer = csv.writer(writeFile)
+		writer.writerows(lines)
+
 
 #_init_storage()
 #_drop_table('table_name_test')
 #_create_table('table_name_test', 0, 2, ['int', 'varchar 20'], ['idd', 'namee'], ['idd', 'namee'])
 #_insert('table_name_test', ['idd', 'namee'], [10, 'asdf'])
-_delete('table_name_test', ['idd', 'namee'], ['>', '='], [12, 'asdf'])
+#_delete('table_name_test', ['idd', 'namee'], ['=', '='], [11, 'asdf'], [13, 'jkl'])
+_update('table_name_test', ['namee', 'idd'], ['=', '='], ['jkl', 13], ['zxcv', 22])
 
 
 
