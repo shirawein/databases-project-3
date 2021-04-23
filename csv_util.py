e02b04f7776ef21212980f685424cc716d74cf1e
Hrishikesh Kulkarni <hrishikeshkulkarni@Hrishikeshs-Air.hsd1.dc.comcast.net>
2021-04-23 19:16:26 -0400

    create index on any column for efficiency

--- csv_util.py
+++ csv_util.py
@@ -1,6 +1,9 @@
 import csv
 import os
 import re
+from BTrees.OOBTree import OOBTree
+from BTrees.IOBTree import IOBTree
+import pickle
 
 def count_rows(tablename):
 	i = 0
@@ -91,6 +94,12 @@ def _init_storage():
 		writer = csv.writer(file, delimiter = ",")
 		writer.writerow(["table_name", "nrows", "ncols", "datatype_list", "primary_key_list"])
 
+def _init_index():
+	with open("index_data.csv", 'a', newline='') as file:
+		writer = csv.writer(file, delimiter = ",")
+		writer.writerow(["index_name", "table_name", "col_name"])
+
+
 def _create_table(table_name, nrows, ncols, datatype_list, colname_list, primary_key_list):
 	with open('table_data.csv') as file:
 		# check for duplicates
@@ -110,6 +119,129 @@ def _create_table(table_name, nrows, ncols, datatype_list, colname_list, primary
 	with open(file_name, 'w') as file:
 		writer = csv.writer(file, delimiter = ",")
 		writer.writerows([colname_list])
+
+def _create_index(index_name, table_name, col_name):
+	with open('index_data.csv') as file:
+		# check for duplicates
+		csv_reader = csv.reader(file, delimiter=',')
+		for row in csv_reader:
+			# print(row)
+			if row[0] == index_name and row[1] == table_name:
+				print("Duplicate entry, cannot create index")
+				return
+		
+		# if no duplicates found write
+	with open('index_data.csv', 'a') as file:
+		writer = csv.writer(file, delimiter = ",")
+		writer.writerow([index_name, table_name, col_name])
+###
+	
+	file_name = table_name + '.csv'
+	with open(file_name, 'r') as readFile:
+		reader = csv.reader(readFile)
+		for row in reader:
+			loc = get_loc(col_name, row)
+			#print(loc)
+			break
+
+	with open('table_data.csv', 'r') as readFile:
+		reader = csv.reader(readFile)
+		for row in reader:
+			if row[0] == table_name:
+				datatype_list = re.findall("'([^']*)'", row[3])
+				#print(datatype_list[loc])
+
+	if datatype_list[loc] == 'int':
+		int_flag = True
+		t = IOBTree()
+	else:
+		int_flag = False
+		t = OOBTree()
+
+###
+	file_name = table_name + '.csv'
+	dictt = {}
+	with open(file_name, 'r') as readFile:
+		reader = csv.reader(readFile)
+		first_row = True
+		loc = 0
+		row_number = -1
+		for row in reader:
+			row_number += 1
+			if first_row:
+				first_row = False
+				loc = get_loc(col_name, row)
+				continue
+#			l = {}
+#			l[row_number] = row_number
+			if int_flag:
+				if int(row[loc]) in dictt:
+					dictt[int(row[loc])][row_number] = row_number
+				else:
+					dictt[int(row[loc])] = {}
+					dictt[int(row[loc])][row_number] = row_number
+			else:
+				if row[loc] in dictt:
+					dictt[row[loc]][row_number] = row_number
+				else:
+					dictt[row[loc]] = {}
+					dictt[row[loc]][row_number] = row_number
+			
+		#print(dictt)
+		t.update(dictt)
+		print(list(t.values()))
+
+
+	# t = OOBTree()
+	# t.update({1: "red", 2: "green", 3: "blue", 4: "spades"})
+	# print(list(t.values()))
+#	config_dictionary = {'remote_hostname': 'google.com', 'remote_port': 80}
+
+	index_file = index_name + '.' + table_name
+	with open(index_file, 'wb') as save_file:
+		pickle.dump(t, save_file)
+
+def _index_test(index_name, table_name):
+	index_file = index_name + '.' + table_name
+	with open(index_file, 'rb') as load_file:
+		t = pickle.load(load_file)
+#		t.update({1119: '9999'})
+		l = list(t.values(max=1400, excludemax=False))
+		for d in l:
+			for key in d:
+				print(key)
+
+#	index_file = index_name + '.' + table_name
+#	with open(index_file, 'wb') as save_file:
+#		pickle.dump(t, save_file)
+
+def _drop_index(table_name, index_name):
+	
+	file_name= table_name + ".csv"
+	index_file = index_name + '.' + table_name
+	# check if table exists 
+	lines = list()
+	flag = 0
+	with open('index_data.csv', 'r') as readFile:
+	    reader = csv.reader(readFile)
+	    for row in reader:
+	        if row[0] == index_name:
+	        	flag = 1
+	        else:
+		        lines.append(row)
+	with open('index_data.csv', 'w') as writeFile:
+	    writer = csv.writer(writeFile)
+	    writer.writerows(lines)
+
+	if flag == 1:
+		if os.path.exists(index_file):
+		    os.remove(index_file)
+		    print("The index: {} is deleted!".format(index_name))
+		else:
+		    print("The index: {} does not exist!".format(index_name))
+	else:
+		print("The index: {} does not exist!".format(index_name))
+
 
 def _drop_table(table_name):
 	
@@ -137,6 +269,7 @@ def _drop_table(table_name):
 		    print("The table: {} does not exist!".format(table_name))
 	else:
 		print("The table: {} does not exist!".format(table_name))
+
 
 def _insert(table_name, colname_list, value_list):
 
@@ -192,10 +325,43 @@ def _insert(table_name, colname_list, value_list):
 		writer = csv.writer(file, delimiter = ",")
 		writer.writerow(list(map(list, zip(*sorted_tuple_list)))[1])
 
+####
+	with open('index_data.csv', 'r') as readFile:
+		reader = csv.reader(readFile)
+		for row in reader:
+			if row[1] == table_name:
+				index_file = row[0] + '.' + table_name
+				index_col = row[2]
+				index_flag = True
+				with open(index_file, 'rb') as load_file:
+							t = pickle.load(load_file)
+
+	if index_flag:
+		dictt = dict(t.items())
+		print(dictt)
+		print(type(dictt))
+		loc = get_loc(index_col, sorted_colname_list)
+		key_value = sorted_tuple_list[loc][1]
+		print(key_value)
+		row_id = count_rows(table_name) - 1
+		if key_value in dictt:
+			dictt[key_value].update({row_id : row_id})
+		else:
+			dictt.update({key_value: {}})
+			dictt[key_value].update({row_id : row_id})
+		
+		print(dictt)
+		t.update(dictt)
+
+		with open(index_file, 'wb') as save_file:
+			pickle.dump(t, save_file)
+
+
+
 def _delete(table_name, colname_list, condition_list, value_list):
 	
 	file_name= table_name + ".csv"
-
+	index_flag = False
 	# check if table exists
 	flag = 0
 	with open('table_data.csv') as readFile:
@@ -215,11 +381,27 @@ def _delete(table_name, colname_list, condition_list, value_list):
 			sorted_colname_list = row
 			break	
 
+####
+	with open('index_data.csv', 'r') as readFile:
+		reader = csv.reader(readFile)
+		for row in reader:
+			if row[1] == table_name:
+				index_file = row[0] + '.' + table_name
+				index_col = row[2]
+				index_flag = True
+				with open(index_file, 'rb') as load_file:
+							t = pickle.load(load_file)
+
+####
+
 	lines = list()
 	with open(file_name, 'r') as readFile:
 		reader = csv.reader(readFile)
 		first_row = True
+		row_id = -1
+		row_ids = []
 		for row in reader:
+			row_id += 1
 			true_flag = 0
 			if(not first_row):
 				for i in range(0, len(colname_list)):
@@ -231,10 +413,35 @@ def _delete(table_name, colname_list, condition_list, value_list):
 				true_flag = 1
 			if true_flag == 1:
 				lines.append(row)
+			else:
+				if index_flag:
+					row_ids.append(row_id)
 
 	with open(file_name, 'w') as writeFile:
 		writer = csv.writer(writeFile)
 		writer.writerows(lines)
+
+
+	if index_flag:
+		dictt = dict(t.items())
+		print(dictt)
+		print(type(dictt))
+		for row_id in row_ids:
+			key_list = list(t.keys())
+			for key in key_list:
+				element_list = list(dictt[key])
+				for element in element_list:
+					if dictt[key][element] > row_id:
+						del dictt[key][element]
+						dictt[key].update({element-1 : element-1})
+					elif dictt[key][element] == row_id:
+						del dictt[key][element]
+		print(dictt)
+		t.update(dictt)
+
+		with open(index_file, 'wb') as save_file:
+			pickle.dump(t, save_file)
+
 
 def _update(table_name, update_colname_list, update_value_list, colname_list, condition_list, value_list, andor):
 	
@@ -259,11 +466,29 @@ def _update(table_name, update_colname_list, update_value_list, colname_list, co
 			sorted_colname_list = row
 			break	
 
+####
+	with open('index_data.csv', 'r') as readFile:
+		reader = csv.reader(readFile)
+		for row in reader:
+			if row[1] == table_name:
+				index_file = row[0] + '.' + table_name
+				index_col = row[2]
+				index_flag = True
+				with open(index_file, 'rb') as load_file:
+							t = pickle.load(load_file)
+
+####
+
 	lines = list()
 	with open(file_name, 'r') as readFile:
 		reader = csv.reader(readFile)
 		first_row = True
+		key_values = []
+		row_ids = []
+		key_old_values = []
+		row_id = -1
 		for row in reader:
+			row_id += 1
 			true_flag = 0
 			false_flag = 0
 			if(not first_row):
@@ -282,12 +507,41 @@ def _update(table_name, update_colname_list, update_value_list, colname_list, co
 			else:
 				for i in range(0, len(update_colname_list)):
 					loc = get_loc(update_colname_list[i], sorted_colname_list)
+					if index_flag and update_colname_list[i] == index_col:
+						row_ids.append(row_id)
+						key_old_values.append(row[loc])
+						key_values.append(update_value_list[i])
 					row[loc] = update_value_list[i]
+					
 				lines.append(row)
 
 	with open(file_name, 'w') as writeFile:
 		writer = csv.writer(writeFile)
 		writer.writerows(lines)
+
+	if index_flag:
+		dictt = dict(t.items())
+		print(dictt)
+		print(type(dictt))
+		i = -1
+		for row_id in row_ids:
+			i += 1
+			del dictt[int(key_old_values[i])][row_id]
+		
+		i = -1
+		for row_id in row_ids:
+			i += 1
+			if key_values[i] in dictt:
+				dictt[key_values[i]].update({row_id : row_id})
+			else:
+				dictt.update({key_values[i]: {}})
+				dictt[key_values[i]].update({row_id : row_id})
+	
+		print(dictt)
+		t.update(dictt)
+
+		with open(index_file, 'wb') as save_file:
+			pickle.dump(t, save_file)
 
 def _select(table_name, view_colname_list, mmcas_list, colname_list, condition_list, value_list, andor):
 	
@@ -316,11 +570,57 @@ def _select(table_name, view_colname_list, mmcas_list, colname_list, condition_l
 	for i in range(0,len(view_colname_list)):
 		view_colname_locs.append(get_loc(view_colname_list[i], sorted_colname_list))
 	
+	
+###
+	new_m_list = []
+	flag_index = False
+	with open('index_data.csv', 'r') as readFile:
+		reader = csv.reader(readFile)
+		if andor != 'or':
+			for row in reader:
+				i = -1
+				for col in colname_list:
+					i += 1
+					if col == row[2] and table_name == row[1]:
+						index_file = row[0] + '.' + table_name
+						with open(index_file, 'rb') as load_file:
+							t = pickle.load(load_file)
+							flag_index = True
+							break
+				if flag_index == True:
+				
+					sign = condition_list[i]
+					value_list[i]
+					if sign == '=':
+						m_list = list(t.values(min=value_list[i], max=value_list[i], excludemin=False, excludemax=False))
+					elif sign == '<':
+						m_list = list(t.values(max=value_list[i], excludemax=True))
+					elif sign == '>':
+						m_list = list(t.values(min=value_list[i], excludemin=True))
+					elif sign == '<=':
+						m_list = list(t.values(max=value_list[i], excludemax=False))
+					elif sign == '>=':
+						m_list = list(t.values(min=value_list[i], excludemin=False))
+
+
+					for d in m_list:
+						for key in d:
+							new_m_list.append(key)
+					break
+
+	if flag_index == False:
+		new_m_list = list(range(1,count_rows(table_name)))
+			
+###
+	print(new_m_list)
 	lines = list()
 	with open(file_name, 'r') as readFile:
 		reader = csv.reader(readFile)
-		first_row = True
-		for row in reader:
+		rows = list(reader)
+		first_row = False
+
+		for m in new_m_list:
+			row = rows[m]
 			true_flag = 0
 			false_flag = 0
 			if(not first_row):
@@ -347,7 +647,7 @@ def _select(table_name, view_colname_list, mmcas_list, colname_list, condition_l
 				for i in range(0, len(view_colname_list)):
 					new_row.append(row[view_colname_locs[i]])
 				lines.append(new_row)
-
+				
 #	print(lines)
 	if mmcas_list:
 		return mmcas_function(lines, mmcas_list)
@@ -507,7 +807,7 @@ def _join(table_name_list, table1_col_list, table2_col_list, matchcol_table1, ma
 
 # select max \( salary \), count \( id \), avg \( salary \), min \( salary \), sum \( id \) from employee where role = 'manager' or id \< 19;
 
-# _select('employee', ['salary', 'id', 'salary', 'salary', 'id'], ['max', 'count', 'avg', 'min', 'sum'], ['role', 'id'], ['=', '<'], ['manager', 19], 'or')
+#_select('employee', ['salary', 'id', 'salary', 'salary', 'id'], ['max', 'count', 'avg', 'min', 'sum'], ['role', 'id'], ['=', '<'], ['manager', 19], 'or')
 
 # update employee set role = 'engineer2' where salary \> 1300 and role = 'engineer';
 
@@ -539,8 +839,21 @@ def _join(table_name_list, table1_col_list, table2_col_list, matchcol_table1, ma
 
 #create table sample2 \( 'id' int, 'name' varchar\(30\), 'role' varchar\(30\), 'salary' int, primary key \( 'id', 'salary' \) \);
 
+#_init_index();
+#_create_index('salary_index', 'employee', 'salary');
+#_index_test('salary_index', 'employee');
 
+#_select('employee', ['id', 'role', 'name'], [], ['salary', 'role'], ['>', '='], [1100, 'engineer'], 'and')
 
+#_select('employee', ['id', 'role', 'name'], [], ['salary'], ['<'], [1100], '')
+#_select('employee', ['id', 'role', 'name', 'salary'], [], ['name', 'salary'], ['=', '<='], ['bbbb', 1400], 'and')
+	
+#_delete('employee', ['id', 'role', 'salary'], ['>', '=', '>'], [30, 'manager', 1800])
 
+#_update('employee', ['salary'], [2100], ['salary', 'role', 'name'], ['>', '=', '='], [1888, 'manager', 'qqqq'], 'and')
+
+#_insert('employee', ['name', 'role', 'salary', 'id'], ['jjkk', 'manager', 1000, 35])
+
+#_drop_index('employee', 'salary_index')
 
 
